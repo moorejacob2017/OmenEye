34d0a7a7684ab6545cc6f34bc91eaebb10b4b9fa
Jacob Moore <moorejacob2017@gmail.com>
2024-05-21 11:20:07 -0500

    Exposed some classes for script usage

--- OmenEye/__init__.py
+++ OmenEye/__init__.py
@@ -1,1 +1,4 @@
+from .WorkerManager import WorkerManager
+from .Canaries import BasicWAFCanary, AdaptiveWAFCanary
+from .ResponseDBManager import ResponseDBManager, DummyResponse
 from .OmenEye import OmenEye
